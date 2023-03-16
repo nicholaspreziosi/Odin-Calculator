6dc4ca2fdc6d43a4eb626dbed3eec550bcedb03b
Nicholas Preziosi <108760764+nicholaspreziosi@users.noreply.github.com>
2023-03-16 14:07:35 -0400

    Set up operator functions

---
+++ script.js
@@ -0,0 +1,21 @@
+function add (firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract (firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply (firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide (firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+function operate (operator, firstNumber, secondNumber) {
+    
+}
+
+
